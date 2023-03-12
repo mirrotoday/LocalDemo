f33ea9c38f086e55c159287c5e9a554f20df4416
EAS <910380566@qq.com>
2023-03-12 20:58:54 +0800

    给历史接口增加接口文档注解

--- src/main/java/com/example/localdemo/controller/MessageController.java
+++ src/main/java/com/example/localdemo/controller/MessageController.java
@@ -3,6 +3,8 @@ package com.example.localdemo.controller;
 import com.example.localdemo.entity.Message;
 import com.example.localdemo.result.ApiResult;
 import com.example.localdemo.service.MessageService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.poi.ss.formula.functions.T;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,22 +16,26 @@ import java.util.List;
  * @date 2023/3/7 15:15a
  * @description TODO
  */
+@Api(tags = "消息集成")
 @RestController
 @RequestMapping("/sys")
 public class MessageController {
     @Resource
     private MessageService messageService;
 
+    @ApiOperation("批量发送邮件")
     @PostMapping("/sendBatchMail")
      public ApiResult<T> sendBatchMail(@RequestBody List<String> mail) {
          int messageCount = messageService.sendMessageMail(mail);
          return new ApiResult<T>().success("邮件发送成功，共计："+messageCount);
      }
+    @ApiOperation("获取邮件发送日志")
     @GetMapping("/getMailLog")
     public ApiResult<List<Message>> getMailLog() {
         List<Message> data = messageService.selectAll();
         return new ApiResult<List<Message>>().result(data,data.size());
     }
+    @ApiOperation("根据ID获取邮件发送日志")
     @GetMapping("/getMailLogById/{id}")
     public ApiResult<List<Message>> getMailLogById(@PathVariable("id") String id) {
         List<Message> data =  messageService.selectOneById(id);
--- src/main/java/com/example/localdemo/controller/TimeQuartzController.java
+++ src/main/java/com/example/localdemo/controller/TimeQuartzController.java
@@ -3,12 +3,15 @@ package com.example.localdemo.controller;
 import com.example.localdemo.entity.Person;
 import com.example.localdemo.entity.Timequartz;
 import com.example.localdemo.service.TimequartzService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
+@Api(tags = "事务测试版实现 ")
 @RequestMapping("/api")
 @RestController
 @Slf4j
@@ -17,23 +20,28 @@ public class TimeQuartzController {
     @Resource
     private TimequartzService timequartzService;
 
+    @ApiOperation("事务创建")
     @PostMapping("/create")
     public String create(@RequestBody Timequartz timequartz) {
         timequartzService.insert(timequartz);
         return "";
     }
+    @ApiOperation("获取所有事务列表")
     @PostMapping("/getList")
     public List<Timequartz> getList(){
         return timequartzService.getList();
     }
+    @ApiOperation("事务删除")
     @PostMapping("/delete")
     public int delete(@RequestBody List<String> number){
         return timequartzService.delete(number);
     }
+    @ApiOperation("事务更新")
     @PostMapping("/update")
     public int update(@RequestBody Timequartz timequartz){
         return timequartzService.updateData(timequartz);
     }
+    @ApiOperation("多数据源测试")
     @GetMapping("/getSqlServerPerson")
     public List<Person> getPersonInfo(){
         return timequartzService.getPersonList();
@@ -43,6 +51,7 @@ public class TimeQuartzController {
      * 数据导出
      * @param response
      */
+    @ApiOperation("数据导出Excel")
     @GetMapping("/exportData")
     public void exportData(HttpServletResponse response){
         timequartzService.export(response);
@@ -50,6 +59,7 @@ public class TimeQuartzController {
     /**
      * 开启事务
      */
+    @ApiOperation("启动事务（待完成）")
     @PostMapping("/begin")
     public void begin(@RequestParam String number){
 
